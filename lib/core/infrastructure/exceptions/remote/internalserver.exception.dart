6b5f5934f5052a7c7c89028efdfe86537d598679
coolbeatz71 <sigmacool@gmail.com>
2025-10-31 19:00:00 +0200

    feat: add internal server exception

---
+++ lib/core/infrastructure/exceptions/remote/internalserver.exception.dart
@@ -0,0 +1,18 @@
+import 'package:http_status/http_status.dart' show HttpStatusCode;
+import 'server.exception.dart' show ServerException;
+import '../../annotations/problem.mappable.annotation.dart';
+
+/// 500 Internal Server Error exception.
+///
+/// Thrown when the API returns 500 status code indicating an internal
+/// server error or unexpected condition.
+@ProblemMappable.remote('InternalServerException')
+class InternalServerException extends ServerException {
+  InternalServerException({
+    required super.type,
+    required super.title,
+    required super.detail,
+    super.instance,
+    super.extensions,
+  }) : super(status: HttpStatusCode.internalServerError);
+}
