ab23849a5b0b0be62b590fcf0f9f4a7e8c80b328
coolbeatz71 <sigmacool@gmail.com>
2025-11-01 18:10:00 +0200

    chore: remove auth application gitkeep

---
+++ lib/modules/auth/application/usecases/signup.usecase.dart
@@ -0,0 +1,25 @@
+import 'package:fpdart/fpdart.dart' show Either;
+
+import '../../../../core/application/usecase/usecase.port.dart' show IUseCase;
+import '../../../../core/domain/failures/failure.dart' show Failure;
+import '../../domain/entities/auth.response.entity.dart' show AuthResponseEntity;
+import '../../presentation/models/signup.credentials.model.dart' show SignUpCredentialsModel;
+import '../repositories/auth.repository.dart' show IAuthRepository;
+
+/// Sign-up use case implementing business logic for user registration.
+///
+/// Orchestrates the signup flow: registers user via repository, stores
+/// authentication token and user data in local storage, and returns authentication
+/// response. Part of the application layer in Clean Architecture, containing
+/// business rules independent of frameworks and UI.
+class SignUpUseCase implements IUseCase<SignUpCredentialsModel, AuthResponseEntity> {
+  final IAuthRepository _authRepository;
+
+  const SignUpUseCase(this._authRepository);
+
+  @override
+  Future<Either<Failure, AuthResponseEntity>> execute(SignUpCredentialsModel credentials) async {
+    final response = await _authRepository.signUp(credentials);
+    return response;
+  }
+}
