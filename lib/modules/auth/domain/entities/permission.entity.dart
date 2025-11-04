a25aee62830fc533929f914e3039999e7b46c416
coolbeatz71 <sigmacool@gmail.com>
2025-11-04 10:10:00 +0200

    refactor: add Equatable to PermissionEntity

--- lib/modules/auth/domain/entities/permission.entity.dart
+++ lib/modules/auth/domain/entities/permission.entity.dart
@@ -1,9 +1,11 @@
+import 'package:equatable/equatable.dart' show Equatable;
+
 /// Permission entity representing granular access rights.
 ///
 /// Domain entity for individual permissions assigned to users or roles.
 /// Permissions define specific actions users can perform on resources,
 /// including ID, resource name, action type, and description.
-class PermissionEntity {
+class PermissionEntity extends Equatable {
   final String id;
   final String resource;
   final String action;
@@ -15,4 +17,7 @@ class PermissionEntity {
     required this.action,
     required this.description,
   });
+
+  @override
+  List<Object?> get props => [id, resource, action, description];
 }
