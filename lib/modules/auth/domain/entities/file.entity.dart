706829199a1a7ff0c2e61e4499444c9dcba92bc1
coolbeatz71 <sigmacool@gmail.com>
2025-11-04 10:05:00 +0200

    refactor: add Equatable to FileEntity

--- lib/modules/auth/domain/entities/file.entity.dart
+++ lib/modules/auth/domain/entities/file.entity.dart
@@ -1,9 +1,11 @@
+import 'package:equatable/equatable.dart' show Equatable;
+
 /// File entity representing uploaded files in the system.
 ///
 /// Domain entity for files (e.g., user avatars, documents, media).
 /// Tracks file metadata and storage information including ID, filenames,
 /// MIME type, storage URL, size in bytes, and deletion status.
-class FileEntity {
+class FileEntity extends Equatable {
   final String id;
   final String fileName;
   final String originalFileName;
@@ -21,4 +23,15 @@ class FileEntity {
     required this.sizeInBytes,
     required this.isDeleted,
   });
+
+  @override
+  List<Object?> get props => [
+    id,
+    fileName,
+    originalFileName,
+    mimeType,
+    storageUrl,
+    sizeInBytes,
+    isDeleted,
+  ];
 }
