1988c6a78b60da2e152a5354acee6f02e934ef4e
coolbeatz71 <sigmacool@gmail.com>
2025-11-07 09:10:00 +0200

    feat: create SignIn BLoC

---
+++ lib/modules/auth/presentation/bloc/signin/signin.bloc.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_bloc/flutter_bloc.dart' show Bloc, Emitter;
+
+import '../../../application/usecases/signin.usecase.dart' show SignInUseCase;
+import 'signin.event.dart' show SignInEvent, SignInSubmitted;
+import 'signin.state.dart' show SignInState, SignInInitial, SignInLoading, SignInSuccess, SignInFailure;
+
+/// BLoC for handling sign in authentication flow.
+///
+/// Manages sign in state and coordinates with SignInUseCase to
+/// authenticate users. Emits different states based on the authentication
+/// result (loading, success, failure).
+class SignInBloc extends Bloc<SignInEvent, SignInState> {
+  final SignInUseCase _signInUseCase;
+
+  SignInBloc(this._signInUseCase) : super(const SignInInitial()) {
+    on<SignInSubmitted>(_onSignInSubmitted);
+  }
+
+  Future<void> _onSignInSubmitted(
+    SignInSubmitted event,
+    Emitter<SignInState> emit,
+  ) async {
+    emit(const SignInLoading());
+
+    final result = await _signInUseCase.execute(event.credentials);
+
+    result.fold(
+      (failure) => emit(SignInFailure(failure)),
+      (authResponse) => emit(SignInSuccess(authResponse)),
+    );
+  }
+}
