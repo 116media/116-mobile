be5cb277fd274dc8023dd371b1bca6dddd48ef05
coolbeatz71 <sigmacool@gmail.com>
2025-11-08 16:20:00 +0200

    refactor: integrate bottom navigation theme into app theme

---
+++ lib/shared/presentation/themes/app.theme.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import '../utils/colors.util.dart' show ColorsUtil;
+import 'sizing.theme.dart' show SizingTheme;
+import 'themedata/bottomnav.themedata.dart' show BottomNavThemeData;
+
+/// Defines the application's theme configurations for both light and dark modes.
+///
+/// This class provides centralized theme definitions using Material Design 3 (Material You)
+/// with custom color schemes, typography, theme extensions, etc.
+///
+class AppTheme {
+  /// Returns the light theme configuration for the application.
+  ///
+  static ThemeData get lightTheme => ThemeData(
+    useMaterial3: true,
+    fontFamily: "Montserrat",
+    scaffoldBackgroundColor: ColorsUtil.neutral50,
+    colorScheme: ColorScheme.light(
+      brightness: Brightness.light,
+      primary: ColorsUtil.primary,
+      surface: ColorsUtil.slate100,
+      secondary: ColorsUtil.secondary,
+      error: ColorsUtil.error,
+    ),
+    bottomNavigationBarTheme: BottomNavThemeData.lightTheme,
+    extensions: <ThemeExtension<dynamic>>[SizingTheme.values],
+  );
+
+  /// Returns the dark theme configuration for the application.
+  ///
+  static ThemeData get darkTheme => ThemeData(
+    useMaterial3: true,
+    fontFamily: "Montserrat",
+    scaffoldBackgroundColor: ColorsUtil.neutral900,
+    colorScheme: ColorScheme.dark(
+      brightness: Brightness.dark,
+      primary: ColorsUtil.primary,
+      surface: ColorsUtil.slate800,
+      secondary: ColorsUtil.secondary,
+      error: ColorsUtil.error,
+    ),
+    bottomNavigationBarTheme: BottomNavThemeData.darkTheme,
+    extensions: <ThemeExtension<dynamic>>[SizingTheme.values],
+  );
+}
