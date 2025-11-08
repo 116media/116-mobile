bbd951c561f6c261d78a52735a82794ac5896deb
coolbeatz71 <sigmacool@gmail.com>
2025-11-08 15:10:00 +0200

    refactor: update signin usecase import path

--- lib/modules/auth/application/usecases/signin.usecase.dart
+++ lib/modules/auth/application/usecases/signin.usecase.dart
@@ -4,7 +4,7 @@ import '../../../../core/application/usecase/usecase.port.dart' show IUseCase;
 import '../../../../core/domain/failures/failure.dart' show Failure;
 import '../../domain/entities/auth.response.entity.dart' show AuthResponseEntity;
 import '../../presentation/models/signin.credentials.model.dart' show SignInCredentialsModel;
-import '../repositories/auth.repository.dart' show IAuthRepository;
+import '../repositories/auth.repository.port.dart' show IAuthRepository;
 
 /// Sign-in use case implementing business logic for user authentication.
 ///
