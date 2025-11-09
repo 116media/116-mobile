b601a5fa9d3ca0cf573b8f59a63fa7ec50c89a40
coolbeatz71 <sigmacool@gmail.com>
2025-11-09 14:25:55 +0200

    refactor: update signup usecase imports

--- lib/modules/auth/application/usecases/signup.usecase.dart
+++ lib/modules/auth/application/usecases/signup.usecase.dart
@@ -1,7 +1,7 @@
 import 'package:fpdart/fpdart.dart' show Either;
 
-import '../../../../core/application/usecase/usecase.port.dart' show IUseCase;
-import '../../../../core/domain/failures/failure.dart' show Failure;
+import '../../../../shared/application/usecases/usecase.port.dart' show IUseCase;
+import '../../../../shared/domain/failures/failure.dart' show Failure;
 import '../../domain/entities/auth.response.entity.dart' show AuthResponseEntity;
 import '../../presentation/models/signup.credentials.model.dart' show SignUpCredentialsModel;
 import '../repositories/auth.repository.port.dart' show IAuthRepository;
