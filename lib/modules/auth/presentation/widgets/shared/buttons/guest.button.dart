e4e3f2369527d0d2d25d26f0061a0c8be6ec48aa
coolbeatz71 <sigmacool@gmail.com>
2025-11-10 09:31:15 +0200

    refactor: update guest button widget imports

--- lib/modules/auth/presentation/widgets/shared/buttons/guest.button.dart
+++ lib/modules/auth/presentation/widgets/shared/buttons/guest.button.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
-import '../../../../../../shared/themes/extensions/build.context.extension.dart';
-import '../../../../../../shared/utils/colors.util.dart' show ColorsUtil;
+import '../../../../../../shared/presentation/themes/extensions/build.context.extension.dart';
+import '../../../../../../shared/presentation/utils/colors.util.dart' show ColorsUtil;
 
 /// A platform-aware outlined button for guest access.
 ///
