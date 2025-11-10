126e83eb9bfe9bf92048cd7a11f84678fb551247
coolbeatz71 <sigmacool@gmail.com>
2025-11-10 09:41:45 +0200

    refactor: update divider with label widget imports

--- lib/modules/auth/presentation/widgets/shared/divider/divider.with.label.widget.dart
+++ lib/modules/auth/presentation/widgets/shared/divider/divider.with.label.widget.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-import '../../../../../../shared/themes/extensions/build.context.extension.dart';
-import '../../../../../../shared/utils/colors.util.dart' show ColorsUtil;
+import '../../../../../../shared/presentation/themes/extensions/build.context.extension.dart';
+import '../../../../../../shared/presentation/utils/colors.util.dart' show ColorsUtil;
 
 /// A horizontal divider with centered text label.
 ///
