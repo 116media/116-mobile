ca1ba863c4c2e061b307579c131566f6ca85cb90
coolbeatz71 <sigmacool@gmail.com>
2025-11-15 04:37:58 +0200

    refactor: import kApiBaseUrl from shared constants

--- lib/shared/infrastructure/service.locator.dart
+++ lib/shared/infrastructure/service.locator.dart
@@ -1,4 +1,6 @@
 import 'package:get_it/get_it.dart' show GetIt;
+import 'package:chopper/chopper.dart' show ChopperClient;
+import '../../../../api/client/api_116.swagger.dart' show Api116, $JsonSerializableConverter;
 
 import '../../modules/auth/infrastructure/dependencies/auth.dependencies.dart'
     show registerAuthDependencies;
@@ -8,6 +10,7 @@ import '../../platform/preferences/infrastructure/dependencies/preferences.depen
     show registerPreferencesDependencies;
 import '../../platform/session/infrastructure/dependencies/session.dependencies.dart'
     show registerSessionDependencies;
+import 'constants/api.constants.dart' show kApiBaseUrl;
 
 /// Service locator for dependency injection.
 ///
@@ -18,6 +21,14 @@ class ServiceLocator {
   ServiceLocator._();
 
   static Future<void> initialize() async {
+    final chopper = ChopperClient(
+      baseUrl: Uri.parse(kApiBaseUrl),
+      converter: $JsonSerializableConverter(),
+      errorConverter: $JsonSerializableConverter(),
+    );
+    sl.registerSingleton<ChopperClient>(chopper);
+    sl.registerSingleton<Api116>(Api116.create(client: chopper));
+
     await registerConnectivityDependencies(sl);
     await registerPreferencesDependencies(sl);
     await registerSessionDependencies(sl);
