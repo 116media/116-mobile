5d76d5d7b0ebb4ae5980c70fbd040df013d67ec5
coolbeatz71 <sigmacool@gmail.com>
2025-11-15 05:14:05 +0200

    refactor: update local cache failures with title and detail fields

--- lib/shared/domain/failures/local/accessdenied.cache.failure.dart
+++ lib/shared/domain/failures/local/accessdenied.cache.failure.dart
@@ -1,13 +1,14 @@
-import '../../../infrastructure/annotations/problem.mappable.annotation.dart' show ProblemMappable;
-import '../../enums/cache.error.enum.dart' show CacheErrorType;
 import 'cache.failure.dart' show CacheFailure;
 
 /// Failure when access to cache is denied.
 ///
 /// Represents permission or security issues preventing access to local storage,
 /// such as file system permissions or encryption key problems.
-@ProblemMappable.local('AccessDeniedCacheFailure')
 class AccessDeniedCacheFailure extends CacheFailure {
-  const AccessDeniedCacheFailure({required super.message, super.instance})
-    : super(type: CacheErrorType.accessDenied, title: 'Cache Access Denied');
+  const AccessDeniedCacheFailure({
+    required super.detail,
+    super.instance,
+    super.traceId,
+    super.timestamp,
+  }) : super(title: 'AccessDeniedCacheException');
 }
--- lib/shared/domain/failures/local/writefailed.cache.failure.dart
+++ lib/shared/domain/failures/local/writefailed.cache.failure.dart
@@ -1,13 +1,14 @@
-import '../../../infrastructure/annotations/problem.mappable.annotation.dart' show ProblemMappable;
-import '../../enums/cache.error.enum.dart' show CacheErrorType;
 import 'cache.failure.dart' show CacheFailure;
 
 /// Failure when writing data to cache fails.
 ///
 /// Represents errors when data could not be persisted to local storage due to
 /// write errors, permission issues, or storage constraints.
-@ProblemMappable.local('WriteFailedCacheFailure')
 class WriteFailedCacheFailure extends CacheFailure {
-  const WriteFailedCacheFailure({required super.message, super.instance})
-    : super(type: CacheErrorType.writeFailed, title: 'Cache Write Failed');
+  const WriteFailedCacheFailure({
+    required super.detail,
+    super.instance,
+    super.traceId,
+    super.timestamp,
+  }) : super(title: 'WriteFailedCacheException');
 }
