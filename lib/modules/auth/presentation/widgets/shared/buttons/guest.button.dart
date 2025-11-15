a1eb95d7629fbbc59032a715da56401bfc689ac3
coolbeatz71 <sigmacool@gmail.com>
2025-11-15 04:46:07 +0200

    style: reorder variable declarations in guest button

--- lib/modules/auth/presentation/widgets/shared/buttons/guest.button.dart
+++ lib/modules/auth/presentation/widgets/shared/buttons/guest.button.dart
@@ -24,8 +24,8 @@ class GuestButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final borderColor = context.isDarkMode ? ColorsUtil.slate600 : ColorsUtil.slate300;
     final textColor = context.isDarkMode ? ColorsUtil.white : ColorsUtil.black;
+    final borderColor = context.isDarkMode ? ColorsUtil.slate600 : ColorsUtil.slate300;
 
     return PlatformWidget(
       cupertino: (context, platform) => Container(
@@ -66,8 +66,8 @@ class GuestButton extends StatelessWidget {
           child: Text(
             text,
             style: context.textTheme.bodyMedium?.copyWith(
-              fontWeight: FontWeight.w600,
               color: textColor,
+              fontWeight: FontWeight.w600,
             ),
           ),
         ),
