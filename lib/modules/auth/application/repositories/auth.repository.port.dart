7b6ea071caa55cde4865dc1ec2478056b7f5a56f
coolbeatz71 <sigmacool@gmail.com>
2025-11-17 01:59:43 +0200

    feat: add verifyOtp method to auth repository interface

--- lib/modules/auth/application/repositories/auth.repository.port.dart
+++ lib/modules/auth/application/repositories/auth.repository.port.dart
@@ -1,9 +1,12 @@
 import 'package:fpdart/fpdart.dart' show Either;
 
 import '../../../../shared/domain/failures/failure.dart' show Failure;
-import '../../domain/entities/auth.response.entity.dart' show AuthResponseEntity;
+import '../../domain/entities/auth-response/auth.response.entity.dart' show AuthResponseEntity;
+import '../../domain/entities/verifyotp-response/verifyotp.response.entity.dart'
+    show VerifyOtpResponseEntity;
 import '../../presentation/models/signin.credentials.model.dart' show SignInCredentialsModel;
 import '../../presentation/models/signup.credentials.model.dart' show SignUpCredentialsModel;
+import '../../presentation/models/verifyotp.credentials.model.dart' show VerifyOtpCredentialsModel;
 
 /// Repository port (interface) for authentication operations.
 ///
@@ -25,4 +28,10 @@ abstract class IAuthRepository {
   /// Returns [Right] with [AuthResponseEntity] on success,
   /// or [Left] with [Failure] on error.
   Future<Either<Failure, AuthResponseEntity>> signUp(SignUpCredentialsModel credentials);
+
+  /// Verifies OTP code for email verification using the [VerifyOtpCredentialsModel].
+  ///
+  /// Returns [Right] with [VerifyOtpResponseEntity] on success,
+  /// or [Left] with [Failure] on error.
+  Future<Either<Failure, VerifyOtpResponseEntity>> verifyOtp(VerifyOtpCredentialsModel credentials);
 }
