cfab72a3a783adb9c74b33cc9b275e5a7c21cbd3
coolbeatz71 <sigmacool@gmail.com>
2025-11-18 00:10:35 +0200

    feat: add network error handling and resendOtp implementation to remote datasource

--- lib/modules/auth/infrastructure/data-sources/auth.remote.datasource.dart
+++ lib/modules/auth/infrastructure/data-sources/auth.remote.datasource.dart
@@ -5,12 +5,19 @@ import '../../../../api/client/api_116.swagger.dart'
         Api116,
         PublicLoginRequest,
         PublicLoginResponse,
+        PublicResendOtpRequest,
+        PublicResendOtpResponse,
         PublicSignUpResponse,
         PublicSignUpRequest,
         PublicVerifyOtpRequest,
         PublicVerifyOtpResponse;
+import '../../../../shared/infrastructure/exceptions/remote/server.exception.dart'
+    show ServerException;
+import '../../../../shared/infrastructure/exceptions/remote/unknown.exception.dart'
+    show UnknownException;
 import '../../../../shared/infrastructure/mappers/problem.mapper.dart' show ProblemMapper;
 import '../../application/data-sources/auth.remote.datasource.port.dart' show IAuthRemoteDataSource;
+import '../../presentation/models/resendotp.credentials.model.dart' show ResendOtpCredentialsModel;
 import '../../presentation/models/signin.credentials.model.dart' show SignInCredentialsModel;
 import '../../presentation/models/signup.credentials.model.dart' show SignUpCredentialsModel;
 import '../../presentation/models/verifyotp.credentials.model.dart' show VerifyOtpCredentialsModel;
@@ -19,7 +26,8 @@ import '../../presentation/models/verifyotp.credentials.model.dart' show VerifyO
 ///
 /// Handles communication with the backend auth endpoints using the
 /// generated [Api116] client. Converts HTTP error responses to typed exceptions
-/// using [ProblemMapper].
+/// using [ProblemMapper]. Network errors (SocketException, timeouts, etc.) are
+/// caught and converted to [UnknownException].
 class AuthRemoteDataSourceImpl implements IAuthRemoteDataSource {
   final Api116 _apiClient;
 
@@ -27,48 +35,85 @@ class AuthRemoteDataSourceImpl implements IAuthRemoteDataSource {
 
   @override
   Future<PublicLoginResponse> signIn(SignInCredentialsModel model) async {
-    final response = await _apiClient.PublicLogin(
-      body: PublicLoginRequest(credentials: model.credentials, password: model.password),
-    );
+    try {
+      final response = await _apiClient.PublicLogin(
+        body: PublicLoginRequest(credentials: model.credentials, password: model.password),
+      );
 
-    if (response.isSuccessful) {
-      return response.body!;
-    } else {
-      throw ProblemMapper.toException(response as Response);
+      if (response.isSuccessful) {
+        return response.body!;
+      } else {
+        throw ProblemMapper.toException(response as Response);
+      }
+    } on ServerException {
+      rethrow;
+    } catch (_) {
+      throw UnknownException();
     }
   }
 
   @override
   Future<PublicSignUpResponse> signUp(SignUpCredentialsModel model) async {
-    final response = await _apiClient.PublicSignUp(
-      body: PublicSignUpRequest(
-        email: model.email,
-        userName: model.userName,
-        password: model.password,
-      ),
-    );
+    try {
+      final response = await _apiClient.PublicSignUp(
+        body: PublicSignUpRequest(
+          email: model.email,
+          userName: model.userName,
+          password: model.password,
+        ),
+      );
 
-    if (response.isSuccessful) {
-      return response.body!;
-    } else {
-      throw ProblemMapper.toException(response as Response);
+      if (response.isSuccessful) {
+        return response.body!;
+      } else {
+        throw ProblemMapper.toException(response as Response);
+      }
+    } on ServerException {
+      rethrow;
+    } catch (_) {
+      throw UnknownException();
     }
   }
 
   @override
   Future<PublicVerifyOtpResponse> verifyOtp(VerifyOtpCredentialsModel model) async {
-    final response = await _apiClient.PublicVerifyOtp(
-      body: PublicVerifyOtpRequest(
-        email: model.email,
-        code: model.otp,
-        purpose: model.purpose.name,
-      ),
-    );
+    try {
+      final response = await _apiClient.PublicVerifyOtp(
+        body: PublicVerifyOtpRequest(
+          email: model.email,
+          code: model.otp,
+          purpose: model.purpose.value,
+        ),
+      );
 
-    if (response.isSuccessful) {
-      return response.body!;
-    } else {
-      throw ProblemMapper.toException(response as Response);
+      if (response.isSuccessful) {
+        return response.body!;
+      } else {
+        throw ProblemMapper.toException(response as Response);
+      }
+    } on ServerException {
+      rethrow;
+    } catch (_) {
+      throw UnknownException();
+    }
+  }
+
+  @override
+  Future<PublicResendOtpResponse> resendOtp(ResendOtpCredentialsModel model) async {
+    try {
+      final response = await _apiClient.PublicResendOtp(
+        body: PublicResendOtpRequest(email: model.email, purpose: model.purpose.value),
+      );
+
+      if (response.isSuccessful) {
+        return response.body!;
+      } else {
+        throw ProblemMapper.toException(response as Response);
+      }
+    } on ServerException {
+      rethrow;
+    } catch (_) {
+      throw UnknownException();
     }
   }
 }
