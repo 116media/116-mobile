ef1018d3350d91cb2f0bebb4e9573fd308bd4bd9
coolbeatz71 <sigmacool@gmail.com>
2025-11-18 00:12:05 +0200

    feat: implement resendOtp in auth remote repository

--- lib/modules/auth/infrastructure/repositories/auth.remote.repository.dart
+++ lib/modules/auth/infrastructure/repositories/auth.remote.repository.dart
@@ -1,14 +1,17 @@
 import 'package:fpdart/fpdart.dart' show Either, Left, Right;
 
 import '../../../../shared/domain/failures/failure.dart' show Failure;
-import '../../../../shared/infrastructure/mappers/problem.mapper.dart' show ProblemMapper;
 import '../../../../shared/infrastructure/exceptions/remote/server.exception.dart'
     show ServerException;
+import '../../../../shared/infrastructure/mappers/problem.mapper.dart' show ProblemMapper;
 import '../../application/data-sources/auth.remote.datasource.port.dart' show IAuthRemoteDataSource;
 import '../../application/repositories/auth.repository.port.dart' show IAuthRepository;
 import '../../domain/entities/auth-response/auth.response.entity.dart' show AuthResponseEntity;
+import '../../domain/entities/resendotp-response/resendotp.response.entity.dart'
+    show ResendOtpResponseEntity;
 import '../../domain/entities/verifyotp-response/verifyotp.response.entity.dart'
     show VerifyOtpResponseEntity;
+import '../../presentation/models/resendotp.credentials.model.dart' show ResendOtpCredentialsModel;
 import '../../presentation/models/signin.credentials.model.dart' show SignInCredentialsModel;
 import '../../presentation/models/signup.credentials.model.dart' show SignUpCredentialsModel;
 import '../../presentation/models/verifyotp.credentials.model.dart' show VerifyOtpCredentialsModel;
@@ -54,6 +57,19 @@ class AuthRemoteRepository implements IAuthRepository {
       final response = await _remoteDataSource.verifyOtp(credentials);
       final verifyOtpEntity = AuthMapper.verifyOtpResponseFromDto(response);
       return Right(verifyOtpEntity);
+    } on ServerException catch (exception) {
+      return Left(ProblemMapper.toFailure(exception));
+    }
+  }
+
+  @override
+  Future<Either<Failure, ResendOtpResponseEntity>> resendOtp(
+    ResendOtpCredentialsModel credentials,
+  ) async {
+    try {
+      final response = await _remoteDataSource.resendOtp(credentials);
+      final resendOtpEntity = AuthMapper.resendOtpResponseFromDto(response);
+      return Right(resendOtpEntity);
     } on ServerException catch (exception) {
       return Left(ProblemMapper.toFailure(exception));
     }
