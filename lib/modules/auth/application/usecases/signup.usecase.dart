29606458b9ab6352104973530010f9cfebbb5fb0
coolbeatz71 <sigmacool@gmail.com>
2025-11-17 02:07:56 +0200

    refactor: update AuthResponseEntity import paths in usecases

--- lib/modules/auth/application/usecases/signup.usecase.dart
+++ lib/modules/auth/application/usecases/signup.usecase.dart
@@ -2,7 +2,7 @@ import 'package:fpdart/fpdart.dart' show Either;
 
 import '../../../../shared/application/usecases/usecase.port.dart' show IUseCase;
 import '../../../../shared/domain/failures/failure.dart' show Failure;
-import '../../domain/entities/auth.response.entity.dart' show AuthResponseEntity;
+import '../../domain/entities/auth-response/auth.response.entity.dart' show AuthResponseEntity;
 import '../../presentation/models/signup.credentials.model.dart' show SignUpCredentialsModel;
 import '../repositories/auth.repository.port.dart' show IAuthRepository;
 
