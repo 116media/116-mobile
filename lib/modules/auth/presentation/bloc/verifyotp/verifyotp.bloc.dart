2bfa04de5277e0b3ce575c4b97dc8b77773a438d
coolbeatz71 <sigmacool@gmail.com>
2025-11-18 00:13:48 +0200

    refactor: remove ResendOtp dependency from VerifyOtpBloc

--- lib/modules/auth/presentation/bloc/verifyotp/verifyotp.bloc.dart
+++ lib/modules/auth/presentation/bloc/verifyotp/verifyotp.bloc.dart
@@ -1,9 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart' show Bloc, Emitter;
 
-import '../../../../../shared/domain/failures/failure.dart' show Failure;
 import '../../../application/usecases/verifyotp.usecase.dart' show VerifyOtpUseCase;
-import '../../../domain/entities/verifyotp-response/verifyotp.response.entity.dart'
-    show VerifyOtpResponseEntity;
 import 'verifyotp.event.dart' show VerifyOtpEvent, VerifyOtpSubmitted;
 import 'verifyotp.state.dart'
     show VerifyOtpState, VerifyOtpInitial, VerifyOtpLoading, VerifyOtpSuccess, VerifyOtpFailure;
@@ -26,8 +23,8 @@ class VerifyOtpBloc extends Bloc<VerifyOtpEvent, VerifyOtpState> {
     final result = await _verifyOtpUseCase.execute(event.credentials);
 
     result.fold(
-      (Failure failure) => emit(VerifyOtpFailure(failure)),
-      (VerifyOtpResponseEntity response) => emit(VerifyOtpSuccess(response)),
+      (failure) => emit(VerifyOtpFailure(failure)),
+      (response) => emit(VerifyOtpSuccess(response)),
     );
   }
 }
