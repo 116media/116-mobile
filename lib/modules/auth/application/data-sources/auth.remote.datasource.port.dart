db15ce99444e4414934715d35721064cd58a9a08
coolbeatz71 <sigmacool@gmail.com>
2025-11-18 19:39:38 +0200

    feat: add forgotPassword method to auth remote datasource port

--- lib/modules/auth/application/data-sources/auth.remote.datasource.port.dart
+++ lib/modules/auth/application/data-sources/auth.remote.datasource.port.dart
@@ -1,9 +1,12 @@
 import '../../../../api/client/api_116.swagger.dart'
     show
+        PublicForgotPasswordResponse,
         PublicLoginResponse,
         PublicResendOtpResponse,
         PublicSignUpResponse,
         PublicVerifyOtpResponse;
+import '../../presentation/models/forgotpassword.credentials.model.dart'
+    show ForgotPasswordCredentialsModel;
 import '../../presentation/models/resendotp.credentials.model.dart' show ResendOtpCredentialsModel;
 import '../../presentation/models/signin.credentials.model.dart' show SignInCredentialsModel;
 import '../../presentation/models/signup.credentials.model.dart' show SignUpCredentialsModel;
@@ -62,4 +65,15 @@ abstract class IAuthRemoteDataSource {
   /// - [ServerException] if the server returns an error response
   /// - [UnknownException] if network is unreachable or other unexpected errors occur
   Future<PublicResendOtpResponse> resendOtp(ResendOtpCredentialsModel credentials);
+
+  /// Initiates a password reset flow by sending an OTP to the user's email.
+  ///
+  /// Calls the PublicForgotPassword API endpoint to generate and send a password reset OTP.
+  ///
+  /// **Returns:** [PublicForgotPasswordResponse] with success status and email.
+  ///
+  /// **Throws:**
+  /// - [ServerException] if the server returns an error response (e.g., email not found)
+  /// - [UnknownException] if network is unreachable or other unexpected errors occur
+  Future<PublicForgotPasswordResponse> forgotPassword(ForgotPasswordCredentialsModel credentials);
 }
