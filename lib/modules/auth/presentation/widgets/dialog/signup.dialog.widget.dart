11c3d4516874839f9e462b93350423b3a53e2ba8
coolbeatz71 <sigmacool@gmail.com>
2025-11-21 15:54:18 +0200

    feat: integrate social sign-in listeners into SignUpDialog

--- lib/modules/auth/presentation/widgets/dialog/signup.dialog.widget.dart
+++ lib/modules/auth/presentation/widgets/dialog/signup.dialog.widget.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart' show BlocListener, BlocProvider;
+import 'package:go_router/go_router.dart';
+import 'package:flutter_bloc/flutter_bloc.dart'
+    show BlocListener, BlocProvider, MultiBlocProvider, MultiBlocListener;
 
+import '../../../../../platform/onboarding/presentation/utils/onboarding.util.dart'
+    show OnboardingUtil;
 import '../../../../../shared/presentation/themes/extensions/build.context.extension.dart';
 import '../../../../../shared/infrastructure/service.locator.dart' show sl;
 import '../../../../../shared/presentation/utils/colors.util.dart' show ColorsUtil;
 import '../../../../../shared/presentation/utils/dialog.util.dart' show DialogUtil;
+import '../../../../home/presentation/constants/home.constants.dart' show kHomeRoutePath;
+import '../../bloc/facebooksignin/facebooksignin.bloc.dart' show FacebookSignInBloc;
+import '../../bloc/facebooksignin/facebooksignin.state.dart'
+    show FacebookSignInState, FacebookSignInSuccess, FacebookSignInFailure;
+import '../../bloc/googlesignin/googlesignin.bloc.dart' show GoogleSignInBloc;
+import '../../bloc/googlesignin/googlesignin.state.dart'
+    show GoogleSignInState, GoogleSignInSuccess, GoogleSignInFailure;
 import '../../bloc/signup/signup.bloc.dart' show SignUpBloc;
 import '../../bloc/signup/signup.state.dart' show SignUpState, SignUpSuccess, SignUpFailure;
 import '../../utils/dialog.utils.dart' show showAuthDialog;
@@ -37,14 +48,48 @@ class _SignUpDialogState extends State<SignUpDialog> {
     }
   }
 
+  Future<void> _googleSignInStateListener(BuildContext ctx, GoogleSignInState state) async {
+    if (state is GoogleSignInSuccess) {
+      await OnboardingUtil.markCompleted();
+
+      if (!ctx.mounted) return;
+
+      Navigator.of(ctx).pop();
+      ctx.go(kHomeRoutePath);
+    } else if (state is GoogleSignInFailure) {
+      DialogUtil.error(context, message: state.failure.detail);
+    }
+  }
+
+  Future<void> _facebookSignInStateListener(BuildContext ctx, FacebookSignInState state) async {
+    if (state is FacebookSignInSuccess) {
+      await OnboardingUtil.markCompleted();
+
+      if (!ctx.mounted) return;
+
+      Navigator.of(ctx).pop();
+      ctx.go(kHomeRoutePath);
+    } else if (state is FacebookSignInFailure) {
+      DialogUtil.error(context, message: state.failure.detail);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final backgroundColor = context.isDarkMode ? ColorsUtil.slate800 : ColorsUtil.slate200;
 
-    return BlocProvider(
-      create: (context) => sl<SignUpBloc>(),
-      child: BlocListener<SignUpBloc, SignUpState>(
-        listener: _signUpStateListener,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => sl<SignUpBloc>()),
+        BlocProvider(create: (context) => sl<GoogleSignInBloc>()),
+        BlocProvider(create: (context) => sl<FacebookSignInBloc>()),
+      ],
+      child: MultiBlocListener(
+        listeners: [
+          BlocListener<SignUpBloc, SignUpState>(listener: _signUpStateListener),
+          BlocListener<GoogleSignInBloc, GoogleSignInState>(listener: _googleSignInStateListener),
+          BlocListener<FacebookSignInBloc, FacebookSignInState>(listener: _facebookSignInStateListener),
+        ],
         child: Scaffold(
           backgroundColor: Colors.transparent,
           body: Align(
