854d33133eef4afa6aa39269abcb66d92f737dbe
coolbeatz71 <sigmacool@gmail.com>
2025-11-21 15:52:34 +0200

    feat: register Google and Facebook auth dependencies in service locator

--- lib/modules/auth/infrastructure/dependencies/auth.dependencies.dart
+++ lib/modules/auth/infrastructure/dependencies/auth.dependencies.dart
@@ -1,17 +1,26 @@
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart' show FacebookAuth;
 import 'package:get_it/get_it.dart' show GetIt;
+import 'package:google_sign_in/google_sign_in.dart' show GoogleSignIn;
 import 'package:hive_ce/hive.dart' show Box, Hive;
 
 import '../../../../api/client/api_116.swagger.dart' show Api116;
 import '../../application/data-sources/auth.local.datasource.port.dart' show IAuthLocalDataSource;
 import '../../application/data-sources/auth.remote.datasource.port.dart' show IAuthRemoteDataSource;
+import '../../application/data-sources/facebook.auth.datasource.port.dart'
+    show IFacebookAuthDataSource;
+import '../../application/data-sources/google.auth.datasource.port.dart' show IGoogleAuthDataSource;
 import '../../application/repositories/auth.repository.port.dart' show IAuthRepository;
+import '../../application/usecases/facebooksignin.usecase.dart' show FacebookSignInUseCase;
 import '../../application/usecases/forgotpassword.usecase.dart' show ForgotPasswordUseCase;
+import '../../application/usecases/googlesignin.usecase.dart' show GoogleSignInUseCase;
 import '../../application/usecases/resendotp.usecase.dart' show ResendOtpUseCase;
 import '../../application/usecases/resetpassword.usecase.dart' show ResetPasswordUseCase;
 import '../../application/usecases/signin.usecase.dart' show SignInUseCase;
 import '../../application/usecases/signup.usecase.dart' show SignUpUseCase;
 import '../../application/usecases/verifyotp.usecase.dart' show VerifyOtpUseCase;
+import '../../presentation/bloc/facebooksignin/facebooksignin.bloc.dart' show FacebookSignInBloc;
 import '../../presentation/bloc/forgotpassword/forgotpassword.bloc.dart' show ForgotPasswordBloc;
+import '../../presentation/bloc/googlesignin/googlesignin.bloc.dart' show GoogleSignInBloc;
 import '../../presentation/bloc/resendotp/resendotp.bloc.dart' show ResendOtpBloc;
 import '../../presentation/bloc/resetpassword/resetpassword.bloc.dart' show ResetPasswordBloc;
 import '../../presentation/bloc/signin/signin.bloc.dart' show SignInBloc;
@@ -20,6 +29,8 @@ import '../../presentation/bloc/verifyotp/verifyotp.bloc.dart' show VerifyOtpBlo
 import '../constants/hive.constants.dart' show kAuthBox;
 import '../data-sources/auth.local.datasource.dart' show AuthLocalDataSource;
 import '../data-sources/auth.remote.datasource.dart' show AuthRemoteDataSourceImpl;
+import '../data-sources/facebook.auth.datasource.dart' show FacebookAuthDataSource;
+import '../data-sources/google.auth.datasource.dart' show GoogleAuthDataSource;
 import '../repositories/auth.cached.repository.dart' show AuthCachedRepository;
 import '../repositories/auth.remote.repository.dart' show AuthRemoteRepository;
 
@@ -34,9 +45,17 @@ Future<void> registerAuthDependencies(GetIt sl) async {
     AuthLocalDataSource(sl<Box<dynamic>>(instanceName: kAuthBox)),
   );
   sl.registerSingleton<IAuthRemoteDataSource>(AuthRemoteDataSourceImpl(sl<Api116>()));
+  sl.registerSingleton<IGoogleAuthDataSource>(GoogleAuthDataSource(GoogleSignIn.instance));
+  sl.registerSingleton<IFacebookAuthDataSource>(FacebookAuthDataSource(FacebookAuth.instance));
 
   // Repository (decorator pattern: cached wraps remote)
-  sl.registerSingleton<AuthRemoteRepository>(AuthRemoteRepository(sl<IAuthRemoteDataSource>()));
+  sl.registerSingleton<AuthRemoteRepository>(
+    AuthRemoteRepository(
+      sl<IAuthRemoteDataSource>(),
+      sl<IGoogleAuthDataSource>(),
+      sl<IFacebookAuthDataSource>(),
+    ),
+  );
   sl.registerSingleton<IAuthRepository>(
     AuthCachedRepository(sl<AuthRemoteRepository>(), sl<IAuthLocalDataSource>()),
   );
@@ -48,6 +67,8 @@ Future<void> registerAuthDependencies(GetIt sl) async {
   sl.registerFactory<ResendOtpUseCase>(() => ResendOtpUseCase(sl<IAuthRepository>()));
   sl.registerFactory<ForgotPasswordUseCase>(() => ForgotPasswordUseCase(sl<IAuthRepository>()));
   sl.registerFactory<ResetPasswordUseCase>(() => ResetPasswordUseCase(sl<IAuthRepository>()));
+  sl.registerFactory<GoogleSignInUseCase>(() => GoogleSignInUseCase(sl<IAuthRepository>()));
+  sl.registerFactory<FacebookSignInUseCase>(() => FacebookSignInUseCase(sl<IAuthRepository>()));
 
   // BLoCs
   sl.registerFactory<SignInBloc>(() => SignInBloc(sl<SignInUseCase>()));
@@ -56,4 +77,6 @@ Future<void> registerAuthDependencies(GetIt sl) async {
   sl.registerFactory<ResendOtpBloc>(() => ResendOtpBloc(sl<ResendOtpUseCase>()));
   sl.registerFactory<ForgotPasswordBloc>(() => ForgotPasswordBloc(sl<ForgotPasswordUseCase>()));
   sl.registerFactory<ResetPasswordBloc>(() => ResetPasswordBloc(sl<ResetPasswordUseCase>()));
+  sl.registerFactory<GoogleSignInBloc>(() => GoogleSignInBloc(sl<GoogleSignInUseCase>()));
+  sl.registerFactory<FacebookSignInBloc>(() => FacebookSignInBloc(sl<FacebookSignInUseCase>()));
 }
