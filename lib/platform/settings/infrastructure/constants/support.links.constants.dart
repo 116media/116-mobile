f8574b85fc734d4d6d97a3d7d8878875240f81ce
coolbeatz71 <sigmacool@gmail.com>
2025-11-26 09:15:00 +0200

    feat: add support links constants for settings

---
+++ lib/platform/settings/infrastructure/constants/support.links.constants.dart
@@ -0,0 +1,9 @@
+/// Support URLs for the app.
+class SupportLinks {
+  SupportLinks._();
+
+  static const String faq = 'https://116music.com/faq';
+  static const String contactEmail = 'mailto:contact@116music.com';
+  static const String termsAndConditions = 'https://116music.com/terms';
+  static const String copyrightCompliance = 'https://116music.com/copyright';
+}
