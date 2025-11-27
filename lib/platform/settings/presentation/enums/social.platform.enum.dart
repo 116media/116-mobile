ed8519584e2e4bae7f907d93d0a0d11871d739ec
coolbeatz71 <sigmacool@gmail.com>
2025-11-27 12:00:00 +0200

    feat: create social platform settings enums

---
+++ lib/platform/settings/presentation/enums/social.platform.enum.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:iconify_flutter/icons/mdi.dart' show Mdi;
+import 'package:iconify_flutter/icons/ri.dart' show Ri;
+
+import '../../../../shared/presentation/utils/colors.util.dart' show ColorsUtil;
+import '../../infrastructure/constants/social.links.constants.dart' show SocialLinks;
+
+/// Supported social media platforms with their associated data.
+///
+/// Each entry defines:
+/// - [color]: The platform's brand color
+/// - [url]: The external profile or channel link
+/// - [icon]: The icon identifier from the icon library
+enum SocialPlatform {
+  youtube(color: ColorsUtil.youtube, url: SocialLinks.youtube, icon: Mdi.youtube),
+  facebook(color: ColorsUtil.facebook, url: SocialLinks.facebook, icon: Mdi.facebook),
+  instagram(color: ColorsUtil.instagram, url: SocialLinks.instagram, icon: Ri.instagram_fill),
+  twitter(color: ColorsUtil.twitter, url: SocialLinks.twitter, icon: Mdi.twitter);
+
+  final Color color;
+  final String url;
+  final String icon;
+
+  const SocialPlatform({required this.color, required this.url, required this.icon});
+}
