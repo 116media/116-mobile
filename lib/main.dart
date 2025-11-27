2f8d6c5815b71bedbd6aa1c96f0ff2569d23ae36
coolbeatz71 <sigmacool@gmail.com>
2025-11-27 13:00:00 +0200

    feat: integrate settings route in main app

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart' show BlocListener, BlocProvider, MultiBlocProvider;
 import 'package:flutter_dotenv/flutter_dotenv.dart' show dotenv;
 import 'package:flutter_native_splash/flutter_native_splash.dart' show FlutterNativeSplash;
-import 'package:go_router/go_router.dart';
+import 'package:go_router/go_router.dart' show GoRouter;
 import 'package:provider/provider.dart' show ChangeNotifierProvider, Consumer, Provider;
 
 import 'platform/connectivity/presentation/bloc/connectivity.bloc.dart' show ConnectivityBloc;
