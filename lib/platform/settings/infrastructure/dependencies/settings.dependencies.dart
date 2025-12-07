df4ab9879bbe17c77d01bfec2b2766141d4cb6b6
coolbeatz71 <sigmacool@gmail.com>
2025-12-07 18:15:49 +0200

    refactor: remove CloudinaryService from setting dependencies

--- lib/platform/settings/infrastructure/dependencies/settings.dependencies.dart
+++ lib/platform/settings/infrastructure/dependencies/settings.dependencies.dart
@@ -3,10 +3,6 @@ import 'package:get_it/get_it.dart' show GetIt;
 import '../../../../api/client/api_116.swagger.dart' show Api116;
 import '../../../../modules/auth/application/data-sources/auth.local.datasource.port.dart'
     show IAuthLocalDataSource;
-import '../../../../shared/application/services/cloudinary/cloudinary.service.port.dart'
-    show CloudinaryService;
-import '../../../../shared/infrastructure/services/cloudinary/cloudinary.service.dart'
-    show CloudinaryServiceImpl;
 import '../../application/data-sources/settings.remote.datasource.port.dart'
     show ISettingsRemoteDataSource;
 import '../../application/repositories/settings.repository.port.dart' show ISettingsRepository;
@@ -20,13 +16,8 @@ import '../repositories/settings.remote.repository.dart' show SettingsRemoteRepo
 
 /// Registers all settings module dependencies.
 Future<void> registerSettingsDependencies(GetIt sl) async {
-  // Services
-  sl.registerSingleton<CloudinaryService>(CloudinaryServiceImpl());
-
   // Data sources
-  sl.registerSingleton<ISettingsRemoteDataSource>(
-    SettingsRemoteDataSourceImpl(sl<Api116>()),
-  );
+  sl.registerSingleton<ISettingsRemoteDataSource>(SettingsRemoteDataSourceImpl(sl<Api116>()));
 
   // Repository (decorator pattern: cached wraps remote)
   sl.registerSingleton<SettingsRemoteRepository>(
@@ -37,19 +28,10 @@ Future<void> registerSettingsDependencies(GetIt sl) async {
   );
 
   // Use cases
-  sl.registerFactory<UpdateProfileUseCase>(
-    () => UpdateProfileUseCase(sl<ISettingsRepository>()),
-  );
-  sl.registerFactory<UpdateAvatarUseCase>(
-    () => UpdateAvatarUseCase(sl<CloudinaryService>(), sl<ISettingsRepository>()),
-  );
+  sl.registerFactory<UpdateProfileUseCase>(() => UpdateProfileUseCase(sl<ISettingsRepository>()));
+  sl.registerFactory<UpdateAvatarUseCase>(() => UpdateAvatarUseCase(sl<ISettingsRepository>()));
 
   // BLoCs
-  sl.registerFactory<UpdateProfileBloc>(
-    () => UpdateProfileBloc(sl<UpdateProfileUseCase>()),
-  );
-  sl.registerFactory<UpdateAvatarBloc>(
-    () => UpdateAvatarBloc(sl<UpdateAvatarUseCase>()),
-  );
+  sl.registerFactory<UpdateProfileBloc>(() => UpdateProfileBloc(sl<UpdateProfileUseCase>()));
+  sl.registerFactory<UpdateAvatarBloc>(() => UpdateAvatarBloc(sl<UpdateAvatarUseCase>()));
 }
-
