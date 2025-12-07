be7548017d8c9fc377c40c46c3e38bbf605a5f75
coolbeatz71 <sigmacool@gmail.com>
2025-12-07 18:23:19 +0200

    style: reformat the codebase

--- lib/platform/settings/presentation/bloc/updateprofile/updateprofile.bloc.dart
+++ lib/platform/settings/presentation/bloc/updateprofile/updateprofile.bloc.dart
@@ -1,8 +1,7 @@
 import 'package:flutter_bloc/flutter_bloc.dart' show Bloc, Emitter;
 
 import '../../../application/usecases/updateprofile.usecase.dart' show UpdateProfileUseCase;
-import 'updateprofile.event.dart'
-    show UpdateProfileEvent, UpdateProfileSubmitted;
+import 'updateprofile.event.dart' show UpdateProfileEvent, UpdateProfileSubmitted;
 import 'updateprofile.state.dart'
     show
         UpdateProfileState,
