fd1daaccc0167f5dfad1c1767d696b5fb16250c1
coolbeatz71 <sigmacool@gmail.com>
2025-12-07 09:00:00 +0200

    feat: register cloudinary service and update avatar dependencies

--- lib/platform/settings/infrastructure/dependencies/settings.dependencies.dart
+++ lib/platform/settings/infrastructure/dependencies/settings.dependencies.dart
@@ -3,10 +3,16 @@ import 'package:get_it/get_it.dart' show GetIt;
 import '../../../../api/client/api_116.swagger.dart' show Api116;
 import '../../../../modules/auth/application/data-sources/auth.local.datasource.port.dart'
     show IAuthLocalDataSource;
+import '../../../../shared/application/services/cloudinary/cloudinary.service.port.dart'
+    show CloudinaryService;
+import '../../../../shared/infrastructure/services/cloudinary/cloudinary.service.dart'
+    show CloudinaryServiceImpl;
 import '../../application/data-sources/settings.remote.datasource.port.dart'
     show ISettingsRemoteDataSource;
 import '../../application/repositories/settings.repository.port.dart' show ISettingsRepository;
+import '../../application/usecases/updateavatar.usecase.dart' show UpdateAvatarUseCase;
 import '../../application/usecases/updateprofile.usecase.dart' show UpdateProfileUseCase;
+import '../../presentation/bloc/updateavatar/updateavatar.bloc.dart' show UpdateAvatarBloc;
 import '../../presentation/bloc/updateprofile/updateprofile.bloc.dart' show UpdateProfileBloc;
 import '../data-sources/settings.remote.datasource.dart' show SettingsRemoteDataSourceImpl;
 import '../repositories/settings.cached.repository.dart' show SettingsCachedRepository;
@@ -14,6 +20,9 @@ import '../repositories/settings.remote.repository.dart' show SettingsRemoteRepo
 
 /// Registers all settings module dependencies.
 Future<void> registerSettingsDependencies(GetIt sl) async {
+  // Services
+  sl.registerSingleton<CloudinaryService>(CloudinaryServiceImpl());
+
   // Data sources
   sl.registerSingleton<ISettingsRemoteDataSource>(
     SettingsRemoteDataSourceImpl(sl<Api116>()),
@@ -31,10 +40,16 @@ Future<void> registerSettingsDependencies(GetIt sl) async {
   sl.registerFactory<UpdateProfileUseCase>(
     () => UpdateProfileUseCase(sl<ISettingsRepository>()),
   );
+  sl.registerFactory<UpdateAvatarUseCase>(
+    () => UpdateAvatarUseCase(sl<CloudinaryService>(), sl<ISettingsRepository>()),
+  );
 
   // BLoCs
   sl.registerFactory<UpdateProfileBloc>(
     () => UpdateProfileBloc(sl<UpdateProfileUseCase>()),
+  );
+  sl.registerFactory<UpdateAvatarBloc>(
+    () => UpdateAvatarBloc(sl<UpdateAvatarUseCase>()),
   );
 }
 
