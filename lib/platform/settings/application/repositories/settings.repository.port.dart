182966c8e32e28c8b9edb0dedef85413b27d5811
coolbeatz71 <sigmacool@gmail.com>
2025-12-15 02:16:49 +0200

    feat: add changePassword method to ISettingsRepository

--- lib/platform/settings/application/repositories/settings.repository.port.dart
+++ lib/platform/settings/application/repositories/settings.repository.port.dart
@@ -1,10 +1,13 @@
 import 'dart:io' show File;
 
+import 'package:cent16/platform/settings/domain/entities/changepassword-response/changepassword.response.entity.dart';
 import 'package:fpdart/fpdart.dart' show Either;
 
 import '../../../../shared/domain/failures/failure.dart' show Failure;
 import '../../domain/entities/profile-response/profile.response.entity.dart'
     show ProfileResponseEntity;
+import '../../presentation/models/changepassword.credentials.model.dart'
+    show ChangePasswordCredentialsModel;
 import '../../presentation/models/profile.model.dart' show ProfileModel;
 
 /// Port interface for settings repository.
@@ -26,4 +29,12 @@ abstract class ISettingsRepository {
   /// Returns [Right] with [ProfileResponseEntity] on success,
   /// or [Left] with [Failure] on error.
   Future<Either<Failure, ProfileResponseEntity>> updateAvatar(File avatarFile);
+
+  /// Changes the authenticated user's password from [ChangePasswordCredentialsModel]
+  ///
+  /// Returns [Right] with [ChangePasswordResponseEntity] on success,
+  /// or [Left] with [Failure] on error.
+  Future<Either<Failure, ChangePasswordResponseEntity>> changePassword(
+    ChangePasswordCredentialsModel credentials,
+  );
 }
