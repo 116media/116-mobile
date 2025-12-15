ec0dc515ce7c82cafebc65c7465ebd82358ab2c3
coolbeatz71 <sigmacool@gmail.com>
2025-12-15 02:18:36 +0200

    feat: implement changePassword in SettingsRemoteRepository

--- lib/platform/settings/infrastructure/repositories/settings.remote.repository.dart
+++ lib/platform/settings/infrastructure/repositories/settings.remote.repository.dart
@@ -9,8 +9,12 @@ import '../../../../shared/infrastructure/mappers/problem.mapper.dart' show Prob
 import '../../application/data-sources/settings.remote.datasource.port.dart'
     show ISettingsRemoteDataSource;
 import '../../application/repositories/settings.repository.port.dart' show ISettingsRepository;
+import '../../domain/entities/changepassword-response/changepassword.response.entity.dart'
+    show ChangePasswordResponseEntity;
 import '../../domain/entities/profile-response/profile.response.entity.dart'
     show ProfileResponseEntity;
+import '../../presentation/models/changepassword.credentials.model.dart'
+    show ChangePasswordCredentialsModel;
 import '../../presentation/models/profile.model.dart' show ProfileModel;
 import '../mappers/settings.mapper.dart' show SettingsMapper;
 
@@ -40,6 +44,19 @@ class SettingsRemoteRepository implements ISettingsRepository {
       final response = await _remoteDataSource.updateAvatar(avatarFile);
       final profileEntity = SettingsMapper.avatarResponseFromDto(response);
       return Right(profileEntity);
+    } on ServerException catch (exception) {
+      return Left(ProblemMapper.toFailure(exception));
+    }
+  }
+
+  @override
+  Future<Either<Failure, ChangePasswordResponseEntity>> changePassword(
+    ChangePasswordCredentialsModel credentials,
+  ) async {
+    try {
+      final response = await _remoteDataSource.changePassword(credentials);
+      final changePasswordEntity = SettingsMapper.changePasswordResponseFromDto(response);
+      return Right(changePasswordEntity);
     } on ServerException catch (exception) {
       return Left(ProblemMapper.toFailure(exception));
     }
