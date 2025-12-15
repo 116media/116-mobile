9ae0b9a78bb6f529492d8cb736a32da572c355cc
coolbeatz71 <sigmacool@gmail.com>
2025-12-15 02:27:25 +0200

    chore: generate slang translation files

--- lib/i18n/strings.g.dart
+++ lib/i18n/strings.g.dart
@@ -6,7 +6,7 @@
 /// Locales: 2
 /// Strings: 416 (208 per locale)
 ///
-/// Built on 2025-12-14 at 23:46 UTC
+/// Built on 2025-12-15 at 00:26 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint, unused_import
