83b9d7bf2a095ff6ed94f73d5c182e5bfab73f50
coolbeatz71 <sigmacool@gmail.com>
2026-01-11 10:36:00 -0400

    refactor: update ClearSessionUseCase to use ISessionStateRepository

--- lib/platform/session/application/usecases/clear.session.usecase.dart
+++ lib/platform/session/application/usecases/clear.session.usecase.dart
@@ -3,14 +3,14 @@ import 'package:fpdart/fpdart.dart' show Either;
 import '../../../../shared/application/usecases/usecase.port.dart' show IUseCase;
 import '../../../../shared/domain/failures/failure.dart' show Failure;
 import '../../domain/entities/session-state/session.state.entity.dart' show SessionStateEntity;
-import '../repositories/session.repository.port.dart' show ISessionRepository;
+import '../repositories/session.state.repository.port.dart' show ISessionStateRepository;
 
 /// Use case for clearing session data (signout).
 ///
 /// Resets the session to initial state, removing authentication and
 /// setting status to guest. Used during signout flow.
 class ClearSessionUseCase implements IUseCase<void, SessionStateEntity> {
-  final ISessionRepository _repository;
+  final ISessionStateRepository _repository;
 
   const ClearSessionUseCase(this._repository);
 
