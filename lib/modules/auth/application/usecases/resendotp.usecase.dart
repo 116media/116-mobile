6388c6abf8f66e84e2d9992690be36ca747cff8c
coolbeatz71 <sigmacool@gmail.com>
2026-01-16 15:45:14 +0200

    docs: remove excessive ISP justification from ResendOtpUseCase

--- lib/modules/auth/application/usecases/resendotp.usecase.dart
+++ lib/modules/auth/application/usecases/resendotp.usecase.dart
@@ -5,22 +5,21 @@ import '../../../../shared/domain/failures/failure.dart' show Failure;
 import '../../domain/entities/resendotp-response/resendotp.response.entity.dart'
     show ResendOtpResponseEntity;
 import '../../presentation/models/resendotp.credentials.model.dart' show ResendOtpCredentialsModel;
-import '../repositories/auth.repository.port.dart' show IAuthRepository;
+import '../repositories/auth.transient.repository.port.dart' show IAuthTransientRepository;
 
 /// Use case for resending OTP verification codes.
 ///
 /// Encapsulates the business logic for requesting a new OTP to be sent
-/// to the user's email. Follows the Clean Architecture pattern by
-/// coordinating between the presentation and infrastructure layers.
+/// to the user's email.
 class ResendOtpUseCase implements IUseCase<ResendOtpCredentialsModel, void> {
-  final IAuthRepository _authRepository;
+  final IAuthTransientRepository _authTransientRepository;
 
-  const ResendOtpUseCase(this._authRepository);
+  const ResendOtpUseCase(this._authTransientRepository);
 
   @override
   Future<Either<Failure, ResendOtpResponseEntity>> execute(
     ResendOtpCredentialsModel credentials,
   ) async {
-    return _authRepository.resendOtp(credentials);
+    return _authTransientRepository.resendOtp(credentials);
   }
 }
