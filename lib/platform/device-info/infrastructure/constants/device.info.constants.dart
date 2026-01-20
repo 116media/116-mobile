c96fa3ee6436220a926873b71880f7758f503261
coolbeatz71 <sigmacool@gmail.com>
2026-01-21 01:08:12 +0200

    chore: create constants for device infos

---
+++ lib/platform/device-info/infrastructure/constants/device.info.constants.dart
@@ -0,0 +1,23 @@
+/// Android system feature indicating a watch device.
+const String kAndroidFeatureWatch = 'android.hardware.type.watch';
+
+/// Android system feature indicating a TV device.
+const String kAndroidFeatureLeanback = 'android.software.leanback';
+
+/// Android system feature indicating an automotive device.
+const String kAndroidFeatureAutomotive = 'android.hardware.type.automotive';
+
+/// iOS device identifier prefix for iPhone.
+const String kIosDeviceIPhone = 'iPhone';
+
+/// iOS device identifier prefix for iPad.
+const String kIosDeviceIPad = 'iPad';
+
+/// iOS device identifier prefix for iPod.
+const String kIosDeviceIPod = 'iPod';
+
+/// iOS device identifier prefix for Apple Watch.
+const String kIosDeviceWatch = 'Watch';
+
+/// iOS device identifier prefix for Apple TV.
+const String kIosDeviceAppleTV = 'AppleTV';
