ae2f33d498a0936367dd312a59aa39f03a9cde0a
coolbeatz71 <sigmacool@gmail.com>
2026-01-21 00:58:56 +0200

    chore: update kApiBaseUrl to use render/deployed Api

--- lib/shared/infrastructure/constants/api.constants.dart
+++ lib/shared/infrastructure/constants/api.constants.dart
@@ -1,8 +1,9 @@
-import 'dart:io' show Platform;
+// import 'dart:io' show Platform;
 
 /// The base URL of the backend API server.
 ///
 /// For Android emulator: use 10.0.2.2 to access localhost
 /// For iOS simulator: use localhost or 127.0.0.1
 /// For physical devices: use your computer's IP address on the network
-final String kApiBaseUrl = Platform.isAndroid ? 'http://10.0.2.2:5025' : 'http://localhost:5025';
+// final String kApiBaseUrl = Platform.isAndroid ? 'http://10.0.2.2:5025' : 'http://localhost:5025';
+final String kApiBaseUrl = 'https://one16-backend.onrender.com';
